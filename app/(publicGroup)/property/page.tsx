19ede2cc265df0ae0a02b3e99f4f876c49055793
monirzkhan <odvutkabbo@gmail.com>
2026-07-22 20:26:12 +0600

    private route authinticated completed

---
+++ app/(publicGroup)/property/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PropertyPage = () => {
+    return (
+        <div>
+            All Property List
+        </div>
+    );
+};
+
+export default PropertyPage;
--- proxy.ts
+++ proxy.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from 'next/server'
 import { NextRequest } from 'next/server'
 import jwt, { JwtPayload } from 'jsonwebtoken'
+import { access } from 'fs'
 
 
 const AUTH_ROUTES = ['/login', '/register']
+const PUBLIC_ROUTES = ['/', '/property']
 // This function can be marked `async` if using `await` inside
 export function proxy(request: NextRequest) {
     const pathName = request.nextUrl.pathname
@@ -18,20 +20,27 @@ export function proxy(request: NextRequest) {
         userRole = decodedToken.role
     }
 
-    if (accessToken && AUTH_ROUTES.includes(pathName)){
-        if(userRole=="TENANT"){
+    if (accessToken && AUTH_ROUTES.includes(pathName)) {
+        if (userRole == "TENANT") {
             return NextResponse.redirect(new URL('/tenant-dashboard', request.url))
         }
-        else if(userRole=="LANDLORD"){
+        else if (userRole == "LANDLORD") {
             return NextResponse.redirect(new URL('/landlord-dashboard', request.url))
         }
-        else if(userRole=="ADMIN"){
+        else if (userRole == "ADMIN") {
             return NextResponse.redirect(new URL('/admin-dashboard', request.url))
         }
         else {
             return NextResponse.redirect(new URL('/', request.url))
         }
-        
+
+    }
+
+    const isPublicRoutes = PUBLIC_ROUTES.some((route) => pathName === route || pathName.startsWith(route + '/'))
+
+    const isAuthRoutes= AUTH_ROUTES.some((route)=> pathName===route || pathName.startsWith(route+'/'))
+    if(!accessToken && !isPublicRoutes && !isAuthRoutes){
+        return NextResponse.redirect(new URL('/login', request.url))
     }
     return NextResponse.next()
 
@@ -46,6 +55,6 @@ export function proxy(request: NextRequest) {
 export const config = {
     matcher: [
         // '/tenant-dashboard/:path*'
-       '/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)'  
+        '/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)'
     ]
 }
