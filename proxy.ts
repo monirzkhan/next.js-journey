0ed5028c9f681cc73adff1889967fac94ac31e83
monirzkhan <odvutkabbo@gmail.com>
2026-07-22 20:03:33 +0600

    login and register route proxy for logged in user

---
+++ proxy.ts
@@ -0,0 +1,51 @@
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+
+const AUTH_ROUTES = ['/login', '/register']
+// This function can be marked `async` if using `await` inside
+export function proxy(request: NextRequest) {
+    const pathName = request.nextUrl.pathname
+    //   return NextResponse.redirect(new URL('/', request.url))
+    const accessToken = request.cookies.get('accessToken')?.value
+    console.log(accessToken);
+
+    const decodedToken = accessToken ? jwt.decode(accessToken) as JwtPayload : null;
+
+    let userRole = null;
+    if (decodedToken) {
+        userRole = decodedToken.role
+    }
+
+    if (accessToken && AUTH_ROUTES.includes(pathName)){
+        if(userRole=="TENANT"){
+            return NextResponse.redirect(new URL('/tenant-dashboard', request.url))
+        }
+        else if(userRole=="LANDLORD"){
+            return NextResponse.redirect(new URL('/landlord-dashboard', request.url))
+        }
+        else if(userRole=="ADMIN"){
+            return NextResponse.redirect(new URL('/admin-dashboard', request.url))
+        }
+        else {
+            return NextResponse.redirect(new URL('/', request.url))
+        }
+        
+    }
+    return NextResponse.next()
+
+
+
+}
+
+
+// Alternatively, you can use a default export:
+// export default function proxy(request: NextRequest) { ... }
+
+export const config = {
+    matcher: [
+        // '/tenant-dashboard/:path*'
+       '/((?!api|_next/static|favicon.ico|_next/image|.*\\.png$).*)'  
+    ]
+}
